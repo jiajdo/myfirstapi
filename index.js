874e37a076affb4704bf92cb451bc5157d5ca214
Jia <jovsofjdo@gmail.com>
2023-08-22 20:55:26 -0700

    Added middleware

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import BOOKS from 'books.js';
+
+const app = express();
+const port = 4000;
+
+//Middleware
+app.use(cors());
+
+//Equivalent to pwd. Informs server of file paths
+const _dirname = path.resolve();

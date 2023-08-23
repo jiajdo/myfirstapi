0a8332f24e097ee7dbd78076254078b835a46dfe
Jia <jovsofjdo@gmail.com>
2023-08-22 21:11:46 -0700

    Added type as module and two get requests

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ app.use(cors());
 
 //Equivalent to pwd. Informs server of file paths
 const _dirname = path.resolve();
+
+//Request for the homepage
+app.get('/', (req, res) => {
+    res.send('Hi, this is my homepage');
+})
+
+//Requests books and converts to JSON format
+app.get('/books', (req, res) => {
+    res.json(BOOKS);
+})
